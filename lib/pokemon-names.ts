4a7fd6f8f0a4b4cb31482c52fba9c57331141cd0
Lukas <LukasHoltbrink@web.de>
2026-05-06 10:24:15 +0200

    pokemon suche

--- lib/pokemon-names.ts
+++ lib/pokemon-names.ts
@@ -10,4 +10,199 @@ export const pokemonDEtoEN: Record<string, string> = {
   schiggy: "squirtle",
   schillok: "wartortle",
   turtok: "blastoise",
+
+  raupy: "caterpie",
+safcon: "metapod",
+smettbo: "butterfree",
+
+hornliu: "weedle",
+kokuna: "kakuna",
+bibor: "beedrill",
+
+taubsi: "pidgey",
+tauboga: "pidgeotto",
+tauboss: "pidgeot",
+
+rattfratz: "rattata",
+rattikarl: "raticate",
+
+habitak: "spearow",
+ibitak: "fearow",
+
+rettan: "ekans",
+arbok: "arbok",
+
+pikachu: "pikachu",
+raichu: "raichu",
+
+sandan: "sandshrew",
+sandamer: "sandslash",
+
+nidoran_f: "nidoran-f",
+nidorina: "nidorina",
+nidoqueen: "nidoqueen",
+
+nidoran_m: "nidoran-m",
+nidorino: "nidorino",
+nidoking: "nidoking",
+
+piepi: "clefairy",
+pixi: "clefable",
+
+vulpix: "vulpix",
+vulnona: "ninetales",
+
+pummeluff: "Jigglypuff",
+knuddeluff: "Wigglytuff",
+
+zubat: "zubat",
+golbat: "golbat",
+
+myrapla: "oddish",
+duflor: "gloom",
+giflor: "vileplume",
+
+paras: "paras",
+parasek: "parasect",
+
+bluzuk: "venonat",
+omot: "venomoth",
+
+digda: "diglett",
+digdri: "dugtrio",
+
+mauzi: "meowth",
+snobilikat: "persian",
+
+enton: "psyduck",
+entoron: "golduck",
+
+menki: "mankey",
+rasaff: "primeape",
+
+fukano: "growlithe",
+arkani: "arcanine",
+
+machollo: "machop",
+machock: "machoke",
+machomei: "machamp",
+
+knofensa: "bellsprout",
+ultrigaria: "Weepinbell",
+sarzenia: "Weepinbell",
+
+nebulak: "gastly",
+alpollo: "haunter",
+gengar: "gengar",
+
+abra: "abra",
+kadabra: "kadabra",
+simsala: "alakazam",
+
+kleinstein: "geodude",
+georok: "graveler",
+geowaz: "golem",
+
+ponita: "ponyta",
+gallopa: "rapidash",
+
+flegmon: "slowpoke",
+lahmus: "slowbro",
+
+magnetilo: "magnemite",
+magneton: "magneton",
+
+porenta: "farfetch'd",
+
+dodu: "doduo",
+dodri: "dodrio",
+
+jurob: "seel",
+jugong: "dewgong",
+
+sleima: "grimer",
+sleimok: "muk",
+
+kokowei: "exeggutor",
+
+krabby: "krabby",
+kingler: "kingler",
+
+voltobal: "voltorb",
+lektrobal: "electrode",
+
+owei: "exeggcute",
+
+traagosso: "cubone",
+knogga: "marowak",
+
+kicklee: "hitmonlee",
+nockchan: "hitmonchan",
+
+schlurp: "lickitung",
+
+smogon: "koffing",
+smogmog: "weezing",
+
+rihorn: "rhyhorn",
+rizeros: "rhydon",
+
+chaneira: "chansey",
+tangela: "tangela",
+
+kangama: "kangaskhan",
+
+seeper: "horsea",
+seemon: "seadra",
+
+goldini: "goldeen",
+golking: "seaking",
+
+sterndu: "staryu",
+starmie: "starmie",
+
+pantimos: "mr. mime",
+
+sichlor: "scyther",
+rossana: "jynx",
+
+elektek: "electabuzz",
+magmar: "magmar",
+
+pinsir: "pinsir",
+tauros: "tauros",
+
+karpador: "magikarp",
+garados: "gyarados",
+
+lapras: "lapras",
+ditto: "ditto",
+
+evoli: "eevee",
+aquana: "vaporeon",
+blitza: "jolteon",
+flamara: "flareon",
+
+porygon: "porygon",
+
+amoroso: "omastar",
+amonitas: "omanyte",
+
+kabuto: "kabuto",
+kabutops: "kabutops",
+
+aerodactyl: "aerodactyl",
+
+relaxo: "snorlax",
+
+arktos: "articuno",
+zapdos: "zapdos",
+lavados: "moltres",
+
+dratini: "dratini",
+dragonir: "dragonair",
+dragoran: "dragonite",
+
+mewtu: "mewtwo",
+mew: "mew",
 }
